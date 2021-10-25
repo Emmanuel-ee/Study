e8307fa72e778d117560b7c6a9237e06ac7762a5
Vishwa <vishurudra@yahoo.co.in>
2021-10-25 22:10:42 +0530

    'added'

--- gs111/src/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from 'react';
-class Welcome extends React.PureComponent {  
-  render() {
-	console.log("Pure component loaded")
-    return <h1>Welcome</h1>
-  }
-}
-
-export default  Welcome
